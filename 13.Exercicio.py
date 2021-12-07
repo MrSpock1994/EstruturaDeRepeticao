704dcf04def1c9c68c6e3b4ddc53bb1a45224366
MrSpock1994 <willpokerpro2016@gmail.com>
2021-12-06 23:07:28 -0300

    First commit

---
+++ 13.Exercicio.py
@@ -0,0 +1,10 @@
+"""
+Faça um programa que peça dois números, base e expoente, calcule e mostre o primeiro número elevado ao segundo número.
+ Não utilize a função de potência da linguagem.
+
+"""
+
+a = int(input('Digite o valor da base: '))
+b = int(input('Digite o valor do expoente: '))
+
+print(f'{a} elevado a {b} é {a**b}')
